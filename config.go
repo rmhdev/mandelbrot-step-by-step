0154e4ef90f5ed888edfc9e9ed17a6a407815f0d
Rober MH <rmh.dev@gmail.com>
2018-09-07 13:26:41 +0700

    pass: in X axis, converting a pixel greater than max returns error

--- config.go
+++ config.go
@@ -15,8 +15,8 @@ type Config struct {
 }
 
 func (c Config) toReal(x int) (float64, error) {
-	if x < 0 {
-		return 0, errors.New(fmt.Sprintf("X is negative!"))
+	if (x < 0) || x >= c.width {
+		return 0, errors.New(fmt.Sprintf("X is out of bounds! Got %d, expected [0..%d]", x, c.width-1))
 	}
 	size := ((c.realMax - c.realMin) / float64(c.width-1))
 
