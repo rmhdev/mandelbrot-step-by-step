de1142e69e7f3d3c9b80fe26a044dd390d348e51
Rober MH <rmh.dev@gmail.com>
2018-09-07 13:06:52 +0700

    refactor tests: convert pixels in X axis to real numbers

--- config_test.go
+++ config_test.go
@@ -2,35 +2,22 @@ package main
 
 import "testing"
 
-func TestMinPixelToReal(t *testing.T) {
-	realMin := -2.5
-	config := Config{10, 10, realMin, 1.0, -1.0, 1.0}
-	result := config.toReal(0)
-	if result != realMin {
-		t.Errorf(
-			"Incorrect config.toReal(%d), got: (%f), expected: (%f)",
-			0, result, realMin)
+func TestToReal(t *testing.T) {
+	tests := []struct {
+		x        int
+		expected float64
+	}{
+		{0, -2.5},
+		{10, 2.5},
+		{5, 0.0},
 	}
-}
-
-func TestLastPixelToReal(t *testing.T) {
-	realMax := 1.0
-	config := Config{10, 10, -2.5, realMax, -1.0, 1.0}
-	result := config.toReal(9)
-	if result != realMax {
-		t.Errorf(
-			"Incorrect config.toReal(%d), got: (%f), expected: (%f)",
-			9, result, realMax)
-	}
-}
-
-func TestSomePixelToReal(t *testing.T) {
 	config := Config{11, 11, -2.5, 2.5, -1.0, 1.0}
-	result := config.toReal(5)
-	expected := 0.0
-	if result != expected {
-		t.Errorf(
-			"Incorrect config.toReal(%d), got: (%f), expected: (%f)",
-			4, result, expected)
+	for _, test := range tests {
+		result := config.toReal(test.x)
+		if result != test.expected {
+			t.Errorf(
+				"Incorrect config.toReal(%d), got: (%f), expected: (%f)",
+				test.x, result, test.expected)
+		}
 	}
 }
