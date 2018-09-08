85e9d1b45574827bb41ba7efddc9c5117584b1f9
Rober MH <rmh.dev@gmail.com>
2018-09-08 14:07:37 +0700

    make use of command-line flags

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	config := Config{101, 41, -2.0, 0.5, -1.0, 1.0}
-	verifier := Verifier{50}
+	width := flag.Int("width", 101, "width")
+	height := flag.Int("height", 41, "height")
+	realMin := flag.Float64("realMin", -2.0, "Minimum value for real part")
+	realMax := flag.Float64("realMax", 0.5, "Maximum value for real part")
+	imagMin := flag.Float64("imagMin", -1.0, "Minimum value for imaginary part")
+	imagMax := flag.Float64("imagMax", 1.0, "Maximum value for imaginary part")
+	iterations := flag.Int("iterations", 50, "maximum number of iterations per pixel")
+
+	flag.Parse() // Don't forget this!
+
+	config := Config{*width, *height, *realMin, *realMax, *imagMin, *imagMax}
+	verifier := Verifier{*iterations}
+
 	realC, imagC := 0.0, 0.0
 	for y := 0; y < config.height; y++ {
 		imagC, _ = config.toImag(y)
