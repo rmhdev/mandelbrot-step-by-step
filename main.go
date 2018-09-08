023d77db75a6aa1102e60159f47edd875829fcdd
Rober MH <rmh.dev@gmail.com>
2018-09-08 13:57:53 +0700

    loop through all pixels and print the Mandelbrot set in the CLI

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
+	config := Config{101, 41, -2.0, 0.5, -1.0, 1.0}
+	verifier := Verifier{50}
+	realC, imagC := 0.0, 0.0
+	for y := 0; y < config.height; y++ {
+		imagC, _ = config.toImag(y)
+		for x := 0; x < config.width; x++ {
+			realC, _ = config.toReal(x)
+			if verifier.isInside(realC, imagC) {
+				fmt.Print("*")
+			} else {
+				fmt.Print("·")
+			}
+		}
+		fmt.Println("")
+	}
 }
