021d084ebbe1e39145174a1db68cdb3501680414
Rober MH <rmh.dev@gmail.com>
2018-09-17 12:21:11 +0700

    generate image and folder if it does not exist

--- exporter.go
+++ exporter.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+	"strings"
+)
 
 type Exporter struct {
 	representation Representation
@@ -21,4 +28,43 @@ func (e Exporter) export() string {
 	}
 
 	return result
+}
+
+type ImageExporter struct {
+	representation Representation
+	folder         string
+	filename       string
+}
+
+func (e ImageExporter) export() (string, error) {
+	image := image.NewRGBA(image.Rect(0, 0, e.representation.width(), e.representation.height()))
+	black := color.RGBA{0, 0, 0, 255}
+	white := color.RGBA{255, 255, 255, 255}
+	color := black
+	for y := 0; y < e.representation.height(); y++ {
+		for x := 0; x < e.representation.width(); x++ {
+			color = white
+			if e.representation.isInside(x, y) {
+				color = black
+			}
+			image.Set(x, y, color)
+		}
+	}
+	// If destination folder does not exist, create it:
+	if _, folderErr := os.Stat(e.folder); os.IsNotExist(folderErr) {
+		folderErr = os.MkdirAll(e.folder, 0755)
+		if folderErr != nil {
+			return "", folderErr
+		}
+	}
+	// Create file using folder+filename, and encode de image in it:
+	resultFilename := strings.Join([]string{e.folder, e.filename}, "/")
+	f, err := os.OpenFile(resultFilename, os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+	png.Encode(f, image)
+
+	return resultFilename, nil
 }
--- exporter_test.go
+++ exporter_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 	"testing"
 )
@@ -19,5 +22,55 @@ func TestStdOutput(t *testing.T) {
 	expected := "····_****_····_"
 	if result != expected {
 		t.Errorf("Incorrect export! got: `%s`, expected: `%s`", result, expected)
+	}
+}
+
+func TestImageCreatesFile(t *testing.T) {
+	representation := CreateRepresentation(4, 3)
+	representation.set(0, 1, true)
+	representation.set(1, 1, true)
+	representation.set(2, 1, true)
+	representation.set(3, 1, true)
+
+	dir, err := ioutil.TempDir("", "rmhdev-mandelbrot")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exporter := ImageExporter{representation, dir, "mandelbrot.png"}
+	result, exportErr := exporter.export()
+	if exportErr != nil {
+		t.Errorf("Unexpected error while exporting! Got: '%t', expected nil", exportErr)
+	}
+	expectedFilename := strings.Join([]string{dir, "mandelbrot.png"}, "/")
+	if result != expectedFilename {
+		t.Errorf("Incorrect filename exported! Got: '%s', expected '%s'", result, expectedFilename)
+	}
+	if _, err2 := os.Stat(expectedFilename); os.IsNotExist(err2) {
+		t.Errorf("Error creating Mandelbrot image, got: '%s'", err2)
+	}
+	defer os.RemoveAll(dir)
+}
+
+func TestImageCreationCreatesFolderIfDoesNotExist(t *testing.T) {
+	representation := CreateRepresentation(4, 3)
+	representation.set(0, 1, true)
+	representation.set(1, 1, true)
+	representation.set(2, 1, true)
+	representation.set(3, 1, true)
+
+	baseDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	expectedDir := strings.Join([]string{baseDir, "new-folder"}, "/")
+	exporter := ImageExporter{representation, expectedDir, "mandelbrot.png"}
+	_, exportErr := exporter.export()
+	if exportErr != nil {
+		t.Errorf("Unexpected error while creating Mandelbrot image, got: '%s'", exportErr)
+	}
+	if _, existsErr := os.Stat(expectedDir); os.IsNotExist(existsErr) {
+		t.Errorf("Folder has not been created! Expected: '%s'", expectedDir)
+
+		defer os.RemoveAll(expectedDir)
 	}
 }
