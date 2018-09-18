a37393d0546c6f07a631494029bd776dd83d18f5
Rober MH <rmh.dev@gmail.com>
2018-09-18 20:49:16 +0700

    improve default values and add more flags to export the resulting set

--- main.go
+++ main.go
@@ -7,19 +7,22 @@ import (
 )
 
 func main() {
-	width := flag.Int("width", 101, "width")
-	height := flag.Int("height", 41, "height")
+	width := flag.Int("width", 800, "width")
+	height := flag.Int("height", 601, "height")
 	realMin := flag.Float64("realMin", -2.0, "Minimum value for real part")
 	realMax := flag.Float64("realMax", 0.5, "Maximum value for real part")
 	imagMin := flag.Float64("imagMin", -1.0, "Minimum value for imaginary part")
 	imagMax := flag.Float64("imagMax", 1.0, "Maximum value for imaginary part")
 	iterations := flag.Int("iterations", 50, "maximum number of iterations per pixel")
+	exporterName := flag.String("exporter", "image", "name of the exporter")
+	folder := flag.String("folder", "mandelbrot", "folder for exporting images")
+	filename := flag.String("filename", "", "name of the image")
 
 	flag.Parse() // Don't forget this!
 
 	config := Config{*width, *height, *realMin, *realMax, *imagMin, *imagMax}
 	representation := config.representation(Verifier{*iterations})
-	exporter, exporterErr := CreateExporter("image", representation, "mandelbrot", "")
+	exporter, exporterErr := CreateExporter(*exporterName, representation, *folder, *filename)
 	if exporterErr != nil {
 		fmt.Print(exporterErr)
 		os.Exit(1)
