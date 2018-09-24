73d8a50597155f2d2239e33ea39b1a51d3cd2851
Rober MH <rmh.dev@gmail.com>
2018-09-24 12:42:19 +0700

    New BobRoss color palette

--- palette.go
+++ palette.go
@@ -11,3 +11,31 @@ func (p BlackWhitePalette) color(v Verification) color.Color {
 	}
 	return color.RGBA{255, 255, 255, 255}
 }
+
+var BobRoss = []color.Color{
+	color.RGBA{0x00, 0x00, 0x00, 0xff}, // Midnight black
+	color.RGBA{0x02, 0x1e, 0x44, 0xff}, // Prussian blue
+	color.RGBA{0x0a, 0x34, 0x10, 0xff}, // Sap green
+	color.RGBA{0x0c, 0x00, 0x40, 0xff}, // Phthalo blue
+	color.RGBA{0x10, 0x2e, 0x3c, 0xff}, // Phthalo green
+	color.RGBA{0x22, 0x1b, 0x15, 0xff}, // Van Dyke brown
+	color.RGBA{0x4e, 0x15, 0x00, 0xff}, // Alizarin crimson
+	color.RGBA{0x5f, 0x2e, 0x1f, 0xff}, // Dark Sienna
+	color.RGBA{0xc7, 0x9b, 0x00, 0xff}, // Yellow ochre
+	color.RGBA{0xdb, 0x00, 0x00, 0xff}, // Bright red
+	color.RGBA{0xff, 0x3c, 0x00, 0xff}, // Cadmium yellow
+	color.RGBA{0xff, 0xb8, 0x00, 0xff}, // Indian yellow
+	color.RGBA{0xff, 0xff, 0xff, 0xff}, // Titanium white
+}
+
+type BobRossPalette struct {
+}
+
+func (p BobRossPalette) color(v Verification) color.Color {
+	if v.isInside {
+		return BobRoss[0]
+	}
+	pos := (v.iterations) % (len(BobRoss) - 1)
+
+	return BobRoss[pos+1]
+}
--- palette_test.go
+++ palette_test.go
@@ -20,3 +20,23 @@ func TestBlackWhitePalette(t *testing.T) {
 		t.Errorf("Incorrect color in BW palette. Got: '%v', expected: '%v'", isNotInsideColor, white)
 	}
 }
+
+func TestBobRossPalette(t *testing.T) {
+	palette := BobRossPalette{}
+	tests := []struct {
+		isInside      bool
+		iterations    int
+		expectedColor color.Color
+		description   string
+	}{
+		{true, 1, color.RGBA{0, 0, 0, 255}, "Point is inside Mandelbrot"},
+		{false, 11, color.RGBA{255, 255, 255, 255}, "Max iteration, and still not inside"},
+		{false, 12, color.RGBA{2, 30, 68, 255}, "Iteration greater than number of colors"},
+	}
+	for _, test := range tests {
+		resultColor := palette.color(Verification{test.isInside, test.iterations})
+		if test.expectedColor != resultColor {
+			t.Errorf("Incorrect color in BobRoss palette (inside: %v, iter: %d). Got: '%v', expected: '%v' (%s)", test.isInside, test.iterations, resultColor, test.expectedColor, test.description)
+		}
+	}
+}
